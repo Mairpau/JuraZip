e7bc1e23ea4b5a61f0eecfab959ae05c4e7907c0
Mairot Paul <paul.mairot@divtec.ch>
2018-11-13 10:42:42 +0100

    adding heading style

--- header.php
+++ header.php
@@ -1,6 +1,14 @@
 <header>
 
   <div name="logo"><img></div>
-  <div></div>
+  <div name="menu">
+    <ul>
+      <li><a href="">Accueil</a></li>
+      <li><a href="">Catalogue</a></li>
+      <li><a href="">Entreprise</a></li>
+      <li><a href="">Contact</a></li>
+      <li><img></li>
+    </ul>
+  </div>
 
 </header>
